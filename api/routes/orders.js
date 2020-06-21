93fcb4246e12de2047acf31d501241919484ffef
Théo Bernard <theo.bernard.94100@gmail.com>
2020-06-21 15:18:00 +0200

    Add some error handlers & some comments

--- api/routes/orders.js
+++ api/routes/orders.js
@@ -1,18 +1,21 @@
 const express = require("express");
 const router = express.Router();
 
+// Show every oders
 router.get("/", (req, res, next) => {
   res.status(200).json({
     message: "Handling GET request to /orders",
   });
 });
 
+// Add a new order
 router.post("/", (req, res, next) => {
   res.status(201).json({
     message: "Handling POST request to /orders",
   });
 });
 
+// Show a specific order
 router.get("/:orderId", (req, res, next) => {
   const id = req.params.orderId;
 
@@ -21,6 +24,7 @@ router.get("/:orderId", (req, res, next) => {
   });
 });
 
+// Cancel a specific order
 router.delete("/:orderId", (req, res, next) => {
   const id = req.params.orderId;
 
--- api/routes/products.js
+++ api/routes/products.js
@@ -1,18 +1,21 @@
 const express = require("express");
 const router = express.Router();
 
+// Show every products
 router.get("/", (req, res, next) => {
   res.status(200).json({
     message: "Handling GET request to /products",
   });
 });
 
+// Add a new product
 router.post("/", (req, res, next) => {
   res.status(201).json({
     message: "Handling POST request to /products",
   });
 });
 
+// Show a specific product
 router.get("/:productId", (req, res, next) => {
   const id = req.params.productId;
 
@@ -21,12 +24,14 @@ router.get("/:productId", (req, res, next) => {
   });
 });
 
+// Edit a specific product
 router.patch("/:productId", (req, res, next) => {
   res.status(200).json({
     message: "Handling PATCH request with an ID to /products",
   });
 });
 
+// Delete a specific product
 router.delete("/:productId", (req, res, next) => {
   res.status(200).json({
     message: "Handling DELETE request with an ID to /products",
